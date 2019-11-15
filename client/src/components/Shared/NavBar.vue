1746da62db9faf73db4ac68cbb96f48da0e94168
Sipann <vauguste92@gmail.com>
2019-11-15 12:22:57 +0100

    App navigation

---
+++ client/src/components/Shared/NavBar.vue
@@ -0,0 +1,105 @@
+<template>
+
+  <v-container fluid>
+
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar dark text>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">Hit It!</h1>
+        </router-link>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item ripple v-for="item in sideNavItems" :key="item.title" :to="item.link">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="primary" dark app>
+    <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+    <v-toolbar-title class="hidden-xs-only">
+      <router-link to="/" tag="span" style="cursor: pointer">HiT iT!</router-link>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn text v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+        <v-icon class="hidden-sm-only" left>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+
+      <v-btn text v-if="user" @click="signoutUser">
+        <v-icon class="hidden-sm-only" left>mdi-exit-to-app</v-icon>Sign Out
+      </v-btn>
+    </v-toolbar-items>
+
+  </v-app-bar>
+  </v-container>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+  data() {
+    return {
+      sideNav: false,
+      user: null,     // TBU When Auth set up
+    };
+  },
+
+  computed: {
+    horizontalNavItems() {
+      let items = [];
+      if (this.user) {
+        items = [
+          { icon: 'mdi-target', title: 'Targets', link: '/targets' },
+        ];
+      } else {
+        items = [
+          { icon: 'mdi-lock-open', title: 'Enter', link: '/enter' },
+        ];
+      }
+      return items;
+    },
+
+    sideNavItems() {
+      let items = [];
+      if (this.user) {
+        items= [
+          { icon: 'mdi-target', title: 'Targets', link: '/targets' },
+          { icon: 'mdi-pencil', title: 'Create Target', link: '/targets/add' },
+          { icon: 'mdi-account', title: 'Profile', link: '/profile' },
+        ];
+      } else {
+        items = [
+           { icon: 'mdi-lock-open', title: 'Enter', link: '/enter' },
+        ];
+      }
+      return items;
+    },
+  },
+
+  methods: {
+    signoutUser() {
+      console.log('sign out User - TBU when auth set up');
+    },
+    
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
+    },
+
+  }
+
+
+
+}
+</script>
